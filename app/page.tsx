9888116920a8f0ad6e117414290db897e987f15e
Jeloves <anjelovaleraecube@icloud.com>
2024-02-15 15:29:51 -0500

    Created topbar component and styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,11 @@
 import Image from "next/image";
-import styles from "@styles/page.module.css";
 import Topbar from "./components/Topbar";
 
 export default function Home() {
 	return (
 		<>
-			<header>
-				<Topbar></Topbar>
-			</header>
-			<main >
-
-      </main>
+			<Topbar></Topbar>
+			<main></main>
 		</>
 	);
 }
